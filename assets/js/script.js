8224832d04df6d9ebfcccfa265a51365360a3d90
Shirly <shirlyd@axioned.com>
2021-09-04 10:01:04 +0530

    js initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,10 @@
-/* Author: 
+// hamburger menu
 
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+var menu = document.querySelector('.menu');
+var bar = document.querySelector('.bar');
+var navUl = menu.querySelector('ul');
+menu.addEventListener('click', function(e){
+    menu.classList.toggle('active');
+    bar.classList.toggle('ham-active');
+    navUl.classList.toggle('active');
+})
